e207818d71ebb029db515eec51c8b94ec768ef81
BillStephens2022 <two4onebill@yahoo.com>
2024-02-06 11:08:03 -0500

    added course code field to form and added custom validator annotation

--- src/main/java/com/billstephens/springdemo/mvc/Customer.java
+++ src/main/java/com/billstephens/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.billstephens.springdemo.mvc;
 
+import com.billstephens.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -16,6 +17,19 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value="TOPS", message="must start with TOPS")
+    // if not value/message passed in, it will use default values which are set up in the
+    // custom annotation
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
